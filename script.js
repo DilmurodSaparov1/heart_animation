223375d55ce8b3b69231cd2a5e63d66ffe19a8e4
Saparov Dilmurod <92588965+DilmurodSaparov1@users.noreply.github.com>
2022-06-30 11:50:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (event) => {
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    bodyEl.appendChild(spanEl);
+    setTimeout(()=>{
+        spanEl.remove();
+    }, 3000)
+})
